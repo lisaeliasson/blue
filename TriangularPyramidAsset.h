66a3544d2e8dc4933a0cc8b279e10d2ad311409e
lisaeliasson <lisa.eliasson@live.com>
2014-04-22 13:36:52 +0100

    add TriangularPyramidAsset.h

---
+++ TriangularPyramidAsset.h
@@ -0,0 +1,20 @@
+#include "GameAsset.h"
+#include "IInterpolator.h"
+
+#ifndef TRIANGULARPYRAMIDASSET_H_
+#define TRIANGULARPYRAMIDASSET_H_
+
+class TriangularPyramidAsset : public GameAsset {
+ public:
+  TriangularPyramidAsset();
+  TriangularPyramidAsset(float x, float y, float z);
+  ~TriangularPyramidAsset();
+
+  virtual void update();
+  virtual void draw();
+  void setInterpolator(shared_ptr<IInterpolator> li);
+ private:
+  shared_ptr<IInterpolator> li;
+};
+
+#endif // TRIANGULARPYRAMIDASSET_H_
